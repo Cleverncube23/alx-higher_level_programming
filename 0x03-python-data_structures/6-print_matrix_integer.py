73456aea222d7b99be2c57e9aa1aa95c699ba848
Cklevor <cleverncube23@gmail.com>
2023-08-13 00:29:52 -0700

    0x03. Python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers"""
+def print_matrix(integers_grid=[[]]):
+  """
+  Display a grid of integers to standard output  
+  """
+
+  for row_index in range(len(integers_grid)):
     
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
-        print()
+    row_length = len(integers_grid[row_index])
+
+    for col_index in range(row_length):
+    
+      if col_index != row_length - 1:
+      
+        end_char = ' '
+      
+      else:
+      
+        end_char = ''
+        
+      print("{:d}".format(integers_grid[row_index][col_index]), end=end_char)
+
+    print("")
